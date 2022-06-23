6609b70f89d2eafda5232577ce4ccd6acbb3cd40
sen-1908 <sen121108@icloud.com>
2022-06-23 10:12:30 +0900

    addDeleteMethod

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('/posts/{post}','PostController@show');
 Route::post('/posts', 'PostController@store');
 
 Route::put('/posts/{post}', 'PostController@update');
+
+Route::delete('/posts/{post}', 'PostController@delete');
